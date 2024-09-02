fff1c541e2727df6eaef9ec20dde1a3351e19788
Lau Perovich <64489452+LauPerovich@users.noreply.github.com>
2024-09-02 10:15:49 -0300

    Create random colors when hover

--- script.js
+++ script.js
@@ -1,17 +1,73 @@
-const createDivs = function(size) {
-    for (let i = 0; i < size; i++) {
-        const container = document.querySelector("#container");
-        const column = document.createElement("div");
-        column.classList.add("col","square");
-        for (let j = 0; j < size; j++) {
-            const row = document.createElement('div');
-            row.classList.add("row", "square");
-            column.appendChild(row);
-        }
-        container.appendChild(column);
+const createGrid = function(size) {
+    
+    for (let i = 0; i < (size * size); i++) {
+        const screen = document.querySelector("#screen");
+        const square = document.createElement("div");
+        square.classList.add("square");
+        //setGridSize(size);
+        square.style.flexBasis = 'calc(500px / '+ size +')';
+        screen.appendChild(square);
+    }
+    hover();
+}
+
+const hover = function() {
+    const hover = document.querySelectorAll(".square");
+    hover.forEach((square => {
+        square.addEventListener("mouseenter", (event) => {
+        square.style.backgroundColor = randomizeColor();
+        });
+        false;
+    }));
+}
+/* check how to solve grid size with a function
+const setGridSize = function(size) {
+    const squareSize = document.querySelectorAll(".square");
+
+    for (let i = 0; i < squareSize.length; i++) {
+        squareSize[i].style.flexBasis = "calc(500px / "+ size +")";
+    }
+}
+*/
+
+const randomizeColor = function() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return "#" + randomColor;
+}
+
+const createSketchPad = function(userSize) {
+    if (userSize > 100 || userSize <= 0 || userSize === '' || userSize === null) {
+        alert("That's not valid. Enter a valid number");
+    } else {
+        createGrid(userSize);
     }
 }
 
-createDivs(16);
+const resetSketchPad = function() {
+    const squares = document.querySelectorAll(".square");
+    for (let i = 0; i < squares.length; i++){
+        squares[i].remove();
+    }
+}
+
+const start = function () {
+    const $btnStart = document.querySelector('#btn-start');
+    $btnStart.addEventListener("click", () => {
+        resetSketchPad();
+        let userSize = +prompt(`Enter number of squares per side you want? (Less than 100)`);
+        createSketchPad(userSize);
+    })
+
+    $btnStart.addEventListener("mouseenter", (event) => {
+        $btnStart.style.color = "red";
+    })
+
+    $btnStart.addEventListener("mouseleave", (event) => {
+        $btnStart.style.color = "black";
+    })
+}
+
+start();
+
 
 
